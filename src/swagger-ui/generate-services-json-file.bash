e6d2fbfc277f5547c232958a348ad42ac40902b1
xiaoweii <xiaoweii@amazom.com>
2023-11-20 10:42:37 +0800

    fix: fix code build issue

--- src/swagger-ui/generate-services-json-file.bash
+++ src/swagger-ui/generate-services-json-file.bash
@@ -4,4 +4,4 @@
 [ -e "src/swagger-ui/services.json" ] && rm src/swagger-ui/services.json
 
 # Replace variables inside template file with environment variables and write output to services.json
-envsubst < src/swagger-ui/services.template.json > src/swagger-ui/services.json
+envsubst < src/swagger-ui/services.template-retail.json > src/swagger-ui/services.json
